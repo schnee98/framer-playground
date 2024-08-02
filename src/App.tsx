518cf786f6a7910f508a4ca41ce8f4c7ebae2a0c
schnee98 <jooeun06161@gmail.com>
2024-08-02 10:53:03 +0900

    feat: framer-motion 예제 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,40 @@
+import { useRef } from "react";
 import "./index.css";
-import { motion, Variants } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
+import { useDimensions } from "./hooks/useDimension";
+import { Navigation } from "./components/menu/Navigation";
+import { MenuToggle } from "./components/menu/MenuToggle";
 
-interface DrawVariants extends Variants {
-  hidden: {
-    pathLength: number;
-    opacity: number;
-  };
-  visible: (i: number) => {
-    pathLength: number;
-    opacity: number;
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
     transition: {
-      pathLength: {
-        delay: number;
-        type: string;
-        duration: number;
-        bounce: number;
-      };
-      opacity: {
-        delay: number;
-        duration: number;
-      };
-    };
-  };
-}
-
-const draw: DrawVariants = {
-  hidden: { pathLength: 0, opacity: 0 },
-  visible: (i: number) => {
-    const delay = 1 + i * 0.5;
-    return {
-      pathLength: 1,
-      opacity: 1,
-      transition: {
-        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-        opacity: { delay, duration: 0.01 },
-      },
-    };
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
   },
 };
 
 export default function App() {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
+
   return (
-    <motion.svg width="600" height="600" viewBox="0 0 600 600" initial="hidden" animate="visible">
-      <motion.circle cx="100" cy="100" r="80" stroke="#ff0055" variants={draw} custom={1} />
-      <motion.line x1="220" y1="30" x2="360" y2="170" stroke="#00cc88" variants={draw} custom={2} />
-      <motion.line x1="220" y1="170" x2="360" y2="30" stroke="#00cc88" variants={draw} custom={2.5} />
-
-      <motion.rect width="140" height="140" x="410" y="30" rx="20" stroke="#0099ff" variants={draw} custom={3} />
-      <motion.circle cx="100" cy="300" r="80" stroke="#0099ff" variants={draw} custom={2} />
-      <motion.line x1="220" y1="230" x2="360" y2="370" stroke="#ff0055" custom={3} variants={draw} />
-
-      <motion.line x1="220" y1="370" x2="360" y2="230" stroke="#ff0055" custom={3.5} variants={draw} />
-      <motion.rect width="140" height="140" x="410" y="230" rx="20" stroke="#00cc88" custom={4} variants={draw} />
-      <motion.circle cx="100" cy="500" r="80" stroke="#00cc88" variants={draw} custom={3} />
-      
-      <motion.line x1="220" y1="430" x2="360" y2="570" stroke="#0099ff" variants={draw} custom={4} />
-      <motion.line x1="220" y1="570" x2="360" y2="430" stroke="#0099ff" variants={draw} custom={4.5} />
-      <motion.rect width="140" height="140" x="410" y="430" rx="20" stroke="#ff0055" variants={draw} custom={5} />
-    </motion.svg>
+    <motion.nav initial={false} animate={isOpen ? "open" : "closed"} custom={height} ref={containerRef}>
+      <motion.div className="background" variants={sidebar} />
+      <Navigation />
+      <MenuToggle toggle={() => toggleOpen()} />
+    </motion.nav>
   );
 }
---
+++ src/hooks/useDimension.ts
@@ -0,0 +1,14 @@
+import { RefObject, useEffect, useRef } from "react";
+
+export const useDimensions = (ref: RefObject<HTMLElement>) => {
+  const dimensions = useRef({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (ref.current) {
+      dimensions.current.width = ref.current.offsetWidth;
+      dimensions.current.height = ref.current.offsetHeight;
+    }
+  }, []);
+
+  return dimensions.current;
+};
